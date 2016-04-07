a628f7ad146bc4c699aae0a8cdce0d9f876494e7
impavidum <impavidum@admins-MacBook-Pro.local>
2016-04-06 22:43:50 -0500

    Tidy code and remove search

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -19,45 +19,15 @@ angular.module('crawlfishApp')
         vm.customKeys = [$mdConstant.KEY_CODE.ENTER];
         vm.toggleCard = toggleCard;
         vm.startCrawl = startCrawl;
+        vm.searchTerm = '';
     
         var BASE_URL = 'https://crawlfish-dev.herokuapp.com';
-
-        //        $scope.filterPinned = false;
-        //        $scope.toggleLeft = buildToggler('left');
-        //        $scope.isOpenLeft = function () {
-        //            return $mdSidenav('left').isOpen();
-        //        };
-        //
-        //        $scope.close = function () {
-        //            $mdSidenav('left').close()
-        //                .then(function () {
-        //                    $log.debug('close left is done');
-        //                });
-        //        };
-        //
-        //        $scope.closeCards = function () {
-        //            console.log('closing cards');
-        //            $scope.myVals.forEach(function (element) {
-        //                element.inProgress = false;
-        //            });
-        //        };
 
         function toggleCard(crawl) {
             crawl.clicked = !crawl.clicked;
             crawl.found = crawl.words_found.length;
             crawl.max = crawl.words.length;
         }
-
-        //        function buildToggler(navID) {
-        //            return function () {
-        //                $mdSidenav(navID)
-        //                    .toggle()
-        //                    .then(function () {
-        //                        $log.debug('toggle ' + navID + ' is done');
-        //                    });
-        //            };
-        //        }
-    
     
         function startCrawl() {
             var tags = '';
